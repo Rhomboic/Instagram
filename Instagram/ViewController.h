ea430e12395878d2b0acf7732cd5c18bfc7d1324
Rhomboic <lynxdroid2@gmail.com>
2022-06-30 13:58:38 -0700

    fixed missing view controller bug

---
+++ Instagram/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Adam Issah on 6/30/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
